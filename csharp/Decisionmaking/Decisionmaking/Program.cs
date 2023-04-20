0d7c101830baadd50307a2027cec920e57d1fa9d
Chitiprolu <l.chitiprolu@dxc.com>
2023-04-20 12:34:19 +0530

    new commit

---
+++ csharp/Decisionmaking/Decisionmaking/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Decisionmaking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int num1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int num2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Third number");
+            int num3 = Convert.ToInt16(Console.ReadLine());
+            if(num1>num2 && num1 > num3)
+            {
+                Console.WriteLine("First is greater");
+            }
+            else if (num2 > num3)
+            {
+                Console.WriteLine("Second is greater");
+            }
+            else
+            {
+                Console.WriteLine("third is greater");
+            }
+
+        }
+    }
+}
---
+++ csharp/vowels/vowels/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace vowels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the alphabet");
+            char ch = Convert.ToChar(Console.ReadLine());
+            if (ch =='a'||ch =='A'||ch =='e'||ch =='E'||ch =='i'||ch =='I'||ch =='o'||ch =='O'||ch =='u'||ch =='U')
+            {
+                Console.WriteLine("It is vowel");
+            }
+            else
+            {
+                Console.WriteLine("it is not vowel");
+            }
+        }
+    }
+}
